036e7abeaa068c6fcaeb19c555bb0989c70fab43
kiranFullStack <kiran.fullstack@outlook.com>
2019-10-05 20:12:36 +0530

    Added Material UI

--- ecom/src/App.js
+++ ecom/src/App.js
@@ -1,10 +1,17 @@
 import React from "react"
-import "./App.css"
+import "./App.scss"
+import Button from "@material-ui/core/Button"
 
 function App() {
   return (
     <div>
-      <h1>Ecom App</h1>
+      <h1>
+        <i class="material-icons">shopping_cart</i> Ecom App for Target with
+        React Hooks, Redux, Material UI, SCSS and Axios
+      </h1>
+      <Button variant="contained" color="primary">
+        Hello World
+      </Button>
     </div>
   )
 }
