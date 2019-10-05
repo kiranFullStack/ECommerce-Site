fcda14a674bd86e9c6bbb6ccf8acd91c384d4387
kiranFullStack <kiran.fullstack@outlook.com>
2019-10-05 23:13:38 +0530

    Routing added React Router

--- ecom/src/App.js
+++ ecom/src/App.js
@@ -1,20 +1,19 @@
 import React from "react"
 import "./App.scss"
-import Button from "@material-ui/core/Button"
 import HomePage from "./pages/HomePage"
 import { Switch, Route, Link } from "react-router-dom"
-import Hats from "./pages/Hats"
+import Shop from "./pages/Shop"
 
 function App() {
   return (
     <div>
+      <nav>
+        <Link to="/">Home</Link>
+        <Link to="/shop">Shop</Link>
+      </nav>
       <Switch>
-        <Route exact path="/">
-          <HomePage />
-        </Route>
-        <Route path="/hats">
-          <Hats />
-        </Route>
+        <Route path="/shop" component={Shop} />
+        <Route path="/" component={HomePage} />
       </Switch>
     </div>
   )
